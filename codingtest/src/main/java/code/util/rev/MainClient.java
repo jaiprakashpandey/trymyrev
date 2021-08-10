57856663636d08ad9e37802b3429a250b5db479a
jaiprakashpandey <jaiprakashpandey@gmail.com>
2021-08-10 16:04:13 -0400

    Update MainClient.java
    pre-condition handle to avoid program fail

--- codingtest/src/main/java/code/util/rev/MainClient.java
+++ codingtest/src/main/java/code/util/rev/MainClient.java
@@ -3,9 +3,12 @@ package code.util.rev;
 public class MainClient {
 
     public static void main(String args[]) {
-        ReverseUtility rv = new ReverseUtility();
-        String result =  rv.myReverseString(args[0]);
-        System.out.println("Reverse of " + args[0] + " is " + result);
-
+        if (args.length == 0) {
+            System.out.println("Nuthing to reverse!!");
+        } else {
+            ReverseUtility rv = new ReverseUtility();
+            String result = rv.myReverseString(args[0]);
+            System.out.println("Reverse of " + args[0] + "is " + result);
+        }
     }
 }
